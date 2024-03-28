8cd98b92c6da018b70e8cee59f357169a6a316c0
Simon <simobilleter@gmail.com>
2024-03-28 17:22:25 +0100

    episode default cover

--- frontend/src/components/Episode.js
+++ frontend/src/components/Episode.js
@@ -1,7 +1,7 @@
 const Episode = ({ episode }) => {
   return (
     <div className="episode-item">
-      <img src={episode.image_url} alt={'episode-poster-' + episode.number} />
+      <img src={episode.image || '/episode-default.jpg'} alt={'episode-poster-' + episode.number} />
       <p>S{String(episode.season.number).padStart(2, '0')}E{String(episode.number).padStart(2, '0')} - {episode.title}</p>
     </div>
   );

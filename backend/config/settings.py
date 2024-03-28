1a4694c2c130eb7b57fafb428a77f90ed7ad4ba0
Simon <simobilleter@gmail.com>
2024-03-28 16:44:19 +0100

    bump page size

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 100,
 }
 
 AUTOT_CONFIG = get_config()

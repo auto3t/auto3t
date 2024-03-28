3fac24882919f028b8766cc889a6de1f1cd529b2
Simon <simobilleter@gmail.com>
2024-03-28 19:55:10 +0100

    extend search result

--- frontend/src/pages/media/Search.js
+++ frontend/src/pages/media/Search.js
@@ -44,11 +44,19 @@ const Search = () => {
         {results.map((result) => (
           <div key={result.id} className='search-result'>
             <div className='search-poster'>
-              <img src={result.image} alt='image-poster' />
+              {result.image && <img src={result.image} alt='image-poster' />}
             </div>
             <div className='search-description'>
               <h2>{result.name}</h2>
+              <span className='smaller'>ID: {result.id}</span>
               <p dangerouslySetInnerHTML={{__html: result.summary}} />
+              <div className='tag-group'>
+                <a href={result.url} target='_blank'>Link</a>
+                <span>Status: {result.status}</span>
+                {result.premiered && <span>Premiered: {result.premiered}</span>}
+                {result.ended && <span>Ended: {result.ended}</span>}
+              </div>
+              {result.genres.length > 0 && <p>Genres: {result.genres.join(', ')}</p>}
             </div>
           </div>
         ))}

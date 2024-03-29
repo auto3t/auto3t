064ee25b508be086d84d478837365dde0ce85ac1
Simon <simobilleter@gmail.com>
2024-03-29 15:57:45 +0100

    episode status

--- frontend/src/components/Episode.js
+++ frontend/src/components/Episode.js
@@ -2,7 +2,12 @@ const Episode = ({ episode }) => {
   return (
     <div className="episode-item">
       <img src={episode.image || '/episode-default.jpg'} alt={'episode-poster-' + episode.number} />
-      <p>S{String(episode.season.number).padStart(2, '0')}E{String(episode.number).padStart(2, '0')} - {episode.title}</p>
+      <p>
+        <span>S{String(episode.season.number).padStart(2, '0')}</span>
+        <span>E{String(episode.number).padStart(2, '0')}</span>
+        <span> - {episode.title}</span>
+        <span> [{episode.status || '-'}]</span>
+      </p>
     </div>
   );
 };

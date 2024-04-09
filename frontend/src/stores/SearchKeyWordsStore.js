15b74a8252fb9b1bfaa21f2e2d37a471f9933484
Simon <simobilleter@gmail.com>
2024-04-09 18:34:49 +0200

    remove unused store

--- frontend/src/stores/SearchKeyWordsStore.js
+++ frontend/src/stores/SearchKeyWordsStore.js
@@ -2,9 +2,7 @@ import { create } from "zustand";
 
 const useSearchKeyWordStore = create((set) => ({
     keywords: [],
-    deletingCategory: null,
     setKeywords: (keywords) => set({keywords}),
-    setDeletingCategory: (deletingCategory) => set({deletingCategory}),
 }));
 
 export default useSearchKeyWordStore;

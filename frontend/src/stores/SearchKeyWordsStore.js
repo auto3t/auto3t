8119004803919831402853011967fc0dffcc60ad
Simon <simobilleter@gmail.com>
2024-04-09 18:58:08 +0200

    new keyword

--- frontend/src/stores/SearchKeyWordsStore.js
+++ frontend/src/stores/SearchKeyWordsStore.js
@@ -3,6 +3,14 @@ import { create } from "zustand";
 const useSearchKeyWordStore = create((set) => ({
     keywords: [],
     setKeywords: (keywords) => set({keywords}),
+    newKeyword: "",
+    setNewKeyword: (newKeywordName) => set({ newKeyword: newKeywordName }),
+    selectedCategory: "",
+    setSelectedCategory: (newSelectedCategory) => set({ selectedCategory: newSelectedCategory }),
+    direction: "include",
+    setDirection: (newDirection) => set({ direction: newDirection }),
+    isDefault: false,
+    setIsDefault: (newDefault) => set({ isDefault: newDefault }),
 }));
 
 export default useSearchKeyWordStore;

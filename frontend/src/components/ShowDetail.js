6bc3ac062b586b43d746be0d7fb32324708e45d2
Simon <simobilleter@gmail.com>
2024-04-25 21:13:43 +0200

    implement showShowDetails

--- frontend/src/components/ShowDetail.js
+++ frontend/src/components/ShowDetail.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import ImageComponent from "./ImageComponent";
 
 export default function ShowDetail({ showDetail }) {
+
+  const [showShowDetails, setShowDetails] = useState(false);
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showShowDetails);
+  }
+
   return (
     <div className="show-detail">
-      <div className="show-poster">
-        <ImageComponent imagePath={showDetail.image} alt='show-poster' />
+      <div className="show-detail-header">
+        <div className="show-poster">
+          <ImageComponent imagePath={showDetail.image} alt='show-poster' />
+        </div>
+        <div className="show-description">
+          <h1>{showDetail.name}</h1>
+          <span className='smaller'>ID: {showDetail.remote_server_id}</span>
+          <p dangerouslySetInnerHTML={{__html: showDetail.description}} />
+        </div>
       </div>
-      <div className="show-description">
-        <h1>{showDetail.name}</h1>
-        <span className='smaller'>ID: {showDetail.remote_server_id}</span>
-        <p dangerouslySetInnerHTML={{__html: showDetail.description}} />
-      </div>
+      <button onClick={toggleShowDetails}>
+        {showShowDetails ? "Hide" : "Configure"}
+      </button>
+      {showShowDetails && (
+        <p>Overwrite Search Name: {showDetail.search_name}</p>
+      )}
     </div>
   )
 }

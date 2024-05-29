698348d3413f79582a6c6bbafb0c18b239d0d2d6
Simon <simobilleter@gmail.com>
2024-05-29 16:14:15 +0200

    add progress bar

--- frontend/src/components/Episode.js
+++ frontend/src/components/Episode.js
@@ -4,6 +4,8 @@ import TimeComponent from "./TimeComponent";
 
 const Episode = ({ episode, showShow = false }) => {
 
+  const validatedProgress = episode.torrent?.progress != null ? Math.min(100, Math.max(0, episode.torrent?.progress)) : null;
+
   const getEpisodeImage = (episode) => {
     if (episode?.image_episode) {
       if (episode.image_episode.image) {
@@ -15,14 +17,22 @@ const Episode = ({ episode, showShow = false }) => {
 
   return (
     <div className="episode-item">
-      <ImageComponent image={getEpisodeImage(episode)} alt={'episode-poster-' + episode.number} />
+      <div className='image-wrap'>
+        <ImageComponent image={getEpisodeImage(episode)} alt={'episode-poster-' + episode.number} />
+        { validatedProgress && (
+          <div className="progress-bar-background">
+            <div className="progress-bar" style={{ width: `${validatedProgress}%` }}>
+              <span className="smaller">{ validatedProgress }%</span>
+            </div>
+          </div>
+        )}
+      </div>
       <div className="tile-description">
         {showShow && (<><Link to={`tv/${episode.season.show.id}`}>{episode.season.show.name}</Link><br/></>)}
         <span>S{String(episode.season.number).padStart(2, '0')}</span>
         <span>E{String(episode.number).padStart(2, '0')}</span>
         <span> - {episode.title}</span>
         <span className="tag-item" title={episode.status_display}>{episode.status || '-'}</span>
-        { episode.torrent?.progress > 0 && (<span>[{episode.torrent.progress}%]</span>)}
         <br />{ episode.status === 'u' && (<TimeComponent timestamp={episode.release_date} />)}
       </div>
     </div>

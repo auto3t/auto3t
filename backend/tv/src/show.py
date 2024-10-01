0581a244c8739e6ec51132e227ccc5ef94b60dc0
Simon <simobilleter@gmail.com>
2024-10-01 10:02:25 +0700

    fix new show art creation return type

--- backend/tv/src/show.py
+++ backend/tv/src/show.py
@@ -35,15 +35,15 @@ class TVMazeShow:
             show = TVShow.objects.create(**show_data)
             image_show = self._get_image_url(response)
             if image_show:
-                show.image_show = Artwork.objects.get_or_create(image_url=image_show)
+                show.image_show, _ = Artwork.objects.get_or_create(image_url=image_show)
 
             episode_fallback = self._get_fallback(response, "background")
             if episode_fallback:
-                show.episode_fallback = Artwork.objects.get_or_create(image_url=episode_fallback)
+                show.episode_fallback, _ = Artwork.objects.get_or_create(image_url=episode_fallback)
 
             season_fallback = self._get_fallback(response, "poster")
             if season_fallback:
-                show.season_fallback = Artwork.objects.get_or_create(image_url=season_fallback)
+                show.season_fallback, _ = Artwork.objects.get_or_create(image_url=season_fallback)
 
             show.save()
 

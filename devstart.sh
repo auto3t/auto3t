7de2c40fca16f2e8f3054e38b3ff0b76bed5cdbc
Simon <simobilleter@gmail.com>
2024-11-08 16:38:59 +0700

    add scheduler, resize windows

--- devstart.sh
+++ devstart.sh
@@ -33,16 +33,20 @@ if [ $? != 0 ]; then
     tmux send-keys -t $SESSION_NAME:"dev.1" "source .venv/bin/activate && cd backend && python manage.py runserver" C-m
 
     # worker
-    tmux split-window -v -t $SESSION_NAME:"dev.0"
+    tmux split-window -v -t $SESSION_NAME:"dev.0" -l 66%
     tmux send-keys -t $SESSION_NAME:"dev.1" "source .venv/bin/activate && cd backend && python manage.py rqworker --with-scheduler default show thumbnails" C-m
 
+    # scheduler
+    tmux split-window -v -t $SESSION_NAME:"dev.1" -l 50%
+    tmux send-keys -t $SESSION_NAME:"dev.2" "source .venv/bin/activate && cd backend && python manage.py rqscheduler" C-m
+
     # react
-    tmux split-window -v -t $SESSION_NAME:"dev.0"
-    tmux send-keys -t $SESSION_NAME:"dev.1" "cd frontend && npm start" C-m
+    tmux split-window -v -t $SESSION_NAME:"dev.3" -l 66%
+    tmux send-keys -t $SESSION_NAME:"dev.4" "cd frontend && npm start" C-m
 
     # shell
-    tmux split-window -v -t $SESSION_NAME:"dev.3"
-    tmux send-keys -t $SESSION_NAME:"dev.4" "source .venv/bin/activate && cd backend && python manage.py shell" C-m
+    tmux split-window -v -t $SESSION_NAME:"dev.4" -l 50%
+    tmux send-keys -t $SESSION_NAME:"dev.5" "source .venv/bin/activate && cd backend && python manage.py shell" C-m
 
 fi
 

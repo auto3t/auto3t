558c96e62aef01ce174ac82328eba829081197d6
Simon <simobilleter@gmail.com>
2024-11-10 16:42:20 +0700

    implement delete schedule

--- frontend/src/components/Schedule.js
+++ frontend/src/components/Schedule.js
@@ -1,12 +1,18 @@
 import { useCallback, useEffect } from "react";
 import useApi from "../hooks/api"
 import useScheduleStore from "../stores/ScheduleStore";
+import TimeComponent from "./TimeComponent";
 
 export default function Schedule() {
 
-  const { get } = useApi();
+  const { get, del } = useApi();
 
-  const { schedules, setSchedules } = useScheduleStore();
+  const {
+    schedules,
+    setSchedules,
+    deletingSchedule,
+    setDeletingSchedule,
+  } = useScheduleStore();
 
   const fetchSchedules = useCallback(async () => {
     const data = await get('scheduler/');
@@ -17,12 +23,63 @@ export default function Schedule() {
     fetchSchedules();
   }, [fetchSchedules]);
 
+  const handleDeleteSchedule = (schedule) => {
+    setDeletingSchedule(schedule);
+  }
+
+  const handleCancleDeleteSchedule = () => {
+    setDeletingSchedule(null);
+  }
+
+  const handleDeleteScheduleConfirm = async () => {
+    try {
+      const data = await del(`scheduler/${deletingSchedule.id}/`);
+      if (data) {
+        setDeletingSchedule(null);
+        fetchSchedules();
+      } else {
+        console.error('Failed to delete schedule');
+      }
+    } catch (error) {
+      console.error('Error deleting schedule:', error);
+    }
+  }
+
   return (
     <>
       <h2>Schedule</h2>
-      {schedules.map((schedule) => (
-        <p>{schedule.job_display}</p>
-      ))}
+      <table className="keyword-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Schedule</th>
+            <th>Next Execution</th>
+            <th><button>Add</button></th>
+          </tr>
+        </thead>
+        <tbody>
+          {schedules.map((schedule) => (
+            <tr key={schedule.id}>
+              <td>{schedule.job_display}</td>
+              <td>{schedule.cron_schedule}</td>
+              <td>{<TimeComponent timestamp={schedule.next_execution} />}</td>
+              <td>
+                {deletingSchedule === schedule ? (
+                  <>
+                    <button onClick={handleDeleteScheduleConfirm}>Confirm</button>
+                    <button onClick={handleCancleDeleteSchedule}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                    <button>Edit</button>
+                    <button onClick={() => handleDeleteSchedule(schedule)}>Delete</button>
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
--- frontend/src/stores/ScheduleStore.js
+++ frontend/src/stores/ScheduleStore.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 const useScheduleStore = create((set) => ({
   schedules: [],
   setSchedules: (schedules) => set({schedules}),
+  deletingSchedule: null,
+  setDeletingSchedule : (deletingSchedule) => set({deletingSchedule}),
 }))
 
 export default useScheduleStore;

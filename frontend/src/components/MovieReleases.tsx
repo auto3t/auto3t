f69fa9e59f3b617ce71c8f623ca5c16ad6d6e90e
Simon <simobilleter@gmail.com>
2025-01-02 21:15:10 +0700

    style release cells

--- frontend/src/components/MovieReleases.tsx
+++ frontend/src/components/MovieReleases.tsx
@@ -40,15 +40,28 @@ const MovieReleases: React.FC<MovieReleasesInterface> = ({ movie_id }) => {
     <>
       <h2>Releases</h2>
       {releases ? (
-        releases.map((release) => (
-          <div key={release.id}>
-            <span className="tag-item">{release.country}</span>
-            <span>{release.release_type_display}</span>
-            <TimeComponent timestamp={release.release_date} />
-            {release.release_lang && <span>{release.release_lang}</span>}
-            {release.note && <span>{release.note}</span>}
+        <div className="movie-release-table">
+          <div className="movie-release-row row-title">
+            <span className="movie-release-cell">Country</span>
+            <span className="movie-release-cell">Medium</span>
+            <span className="movie-release-cell">Date</span>
+            <span className="movie-release-cell">Language</span>
+            <span className="movie-release-cell">Note</span>
           </div>
-        ))
+          {releases.map((release) => (
+            <div key={release.id} className="movie-release-row">
+              <span className="movie-release-cell">{release.country}</span>
+              <span className="movie-release-cell">
+                {release.release_type_display}
+              </span>
+              <span className="movie-release-cell">
+                <TimeComponent timestamp={release.release_date} />
+              </span>
+              <span className="movie-release-cell">{release.release_lang}</span>
+              <span className="movie-release-cell">{release.note}</span>
+            </div>
+          ))}
+        </div>
       ) : (
         <p>No release found.</p>
       )}

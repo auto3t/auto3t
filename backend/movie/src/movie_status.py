717ac38c2dbf1669fa0ab147640d101849aceb3b
Simon <simobilleter@gmail.com>
2025-05-21 21:35:33 +0700

    set movie status

--- backend/movie/src/movie_status.py
+++ backend/movie/src/movie_status.py
@@ -1,6 +1,8 @@
 """check movies"""
 
-from autot.models import Torrent
+from movie.models import Movie
+
+from autot.models import log_change
 
 
 class MovieStatus:
@@ -8,7 +10,20 @@ class MovieStatus:
 
     def refresh(self) -> bool:
         """refresh all movie status"""
+        self.set_upcoming()
 
-        plain_torrents = Torrent.objects.filter(torrent_state="u").exists()
+        # plain_torrents = Torrent.objects.filter(torrent_state="u").exists()
 
-        return any([plain_torrents])
+        return False
+
+    def set_upcoming(self):
+        """set upcoming movie state"""
+        to_update = Movie.objects.filter(status__isnull=True)
+        if not to_update:
+            return
+
+        for movie in to_update:
+            old_status = movie.status
+            movie.status = "u"
+            movie.save()
+            log_change(movie, "u", field_name="status", old_value=old_status, new_value="u")

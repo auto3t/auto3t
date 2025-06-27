4dce40211eb2dacc62b07ed1706e5064ca7a44f9
Simon <simobilleter@gmail.com>
2025-06-27 20:28:28 +0700

    run download_watcher at astartup

--- backend/config/management/commands/start_schedule.py
+++ backend/config/management/commands/start_schedule.py
@@ -1,8 +1,12 @@
 """start schedules"""
 
+from datetime import timedelta
+
 from django.core.management.base import BaseCommand
+from django_rq.queues import get_queue
 
 from autot.models import AutotScheduler
+from autot.tasks import download_watcher
 
 
 class Command(BaseCommand):
@@ -15,3 +19,6 @@ class Command(BaseCommand):
         for schedule in all_schedules:
             schedule.create_on_scheduler()
             self.stdout.write(self.style.SUCCESS(f'Created schedule: "{schedule.job}": {schedule.cron_schedule}'))
+
+        queue = get_queue("default")
+        queue.enqueue_in(timedelta(seconds=30), download_watcher)

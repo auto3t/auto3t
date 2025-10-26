8c032e3d2ae427f987538377231e00c31ddb3f3b
Simon <simobilleter@gmail.com>
2025-10-26 09:22:25 +0700

    add refresh_people task

--- backend/people/tasks.py
+++ backend/people/tasks.py
@@ -1,10 +1,30 @@
 """people tasks"""
 
+from datetime import timedelta
+
+from django.db.models import Q
 from django.utils.timezone import localtime
 from django_rq import job
 from people.models import Person
 from people.src.movie_person import MovieDBPerson
 from people.src.tv_person import TVPerson
+
+from autot.tasks import download_thumbnails
+
+
+@job
+def refresh_people(outdated: int = 30) -> None:
+    """refresh people outdated"""
+    to_refresh = Person.objects.filter(
+        Q(last_refresh__isnull=True) | Q(last_refresh__gte=localtime() + timedelta(days=outdated))
+    )
+
+    person_tasks: list = []
+    for person in to_refresh:
+        person_task = refresh_single_person.delay(person_id=person.id)
+        person_tasks.append(person_task)
+
+    download_thumbnails.delay(depends_on=person_tasks)  # type: ignore
 
 
 @job("people")

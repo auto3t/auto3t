67892c20372dfb0e538659767f1819fb04ca4cca
Simon <simobilleter@gmail.com>
2025-11-05 16:01:21 +0700

    remove content serializer from credit

--- backend/people/serializers.py
+++ backend/people/serializers.py
@@ -3,7 +3,6 @@
 from artwork.serializers import ArtworkSerializer
 from people.models import Credit, Person
 from rest_framework import serializers
-from tv.serializers import TVShowSerializer
 
 
 class PersonSerializer(serializers.ModelSerializer):
@@ -22,27 +21,11 @@ class PersonSerializer(serializers.ModelSerializer):
 class CreditSerializer(serializers.ModelSerializer):
     """serialize credit"""
 
-    SERIALIZER_MAP = {
-        ("tv", "tvshow"): TVShowSerializer,
-    }
-
     person = PersonSerializer()
     content_type_display = serializers.CharField(source="content_type.name")
     content_type_str = serializers.CharField(source="content_type.model")
     role_display = serializers.CharField(source="get_role_display")
-    content_object = serializers.SerializerMethodField()
 
     class Meta:
         model = Credit
         fields = "__all__"
-
-    def get_content_object(self, obj):
-        """dynamic content object serializer"""
-        ct = obj.content_type
-        key = (ct.app_label, ct.model)
-        serializer_class = self.SERIALIZER_MAP.get(key)
-
-        if serializer_class and obj.content_object:
-            return serializer_class(obj.content_object, context=self.context).data
-
-        return {"app_label": ct.app_label, "model": ct.model, "id": obj.object_id}

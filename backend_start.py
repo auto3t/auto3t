7152afb228ea1bd6b08ce2f8a43741dbebd9cb04
Simon <simobilleter@gmail.com>
2025-11-25 21:10:25 +0700

    add A3T_PORT

--- backend_start.py
+++ backend_start.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "config.asgi:application",
         host="0.0.0.0",
-        port=8000,
+        port=int(environ.get("A3T_PORT", 8000)),
         workers=4,
         log_level=LOG_LEVEL,
         reload=False,
